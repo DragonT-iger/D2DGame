f25f1f06de36190b5f4633481f63e078997dee3a
DevEuni <silverlight0522@gmail.com>
2025-07-28 15:09:02 +0900

    soundmanager commit

--- D2DEngine/SoundManager.cpp
+++ D2DEngine/SoundManager.cpp
@@ -1,29 +1,49 @@
 #include "pch.h"
+#include <filesystem>
 #include "SoundManager.h"
+#include "Fmod_Error.h"
+//bgm, sfx, ui 별로 맵 만들고 시스템 init 
+
+SoundManager& SoundManager::Instance()
+{
+	static SoundManager inst;
+	return inst;
+}
 
 bool SoundManager::Init()
 {
-	FMOD::System* SoundManager = nullptr;
+	FMOD_RESULT result = FMOD::System_Create(&m_coreSystem);
+	if (result != FMOD_OK) { FMOD_LOG(result); FMOD_ASSERT(result); return false; }
 
-	FMOD_CHECK(FMOD::System_Create(&SoundManager));
+	//3D Sound 추가시 수정
+	result = m_coreSystem->init(64, FMOD_INIT_NORMAL, nullptr);
+	if (result != FMOD_OK) { FMOD_LOG(result); FMOD_ASSERT(result); return false; }
+
+	return true;
 }
 
 void SoundManager::Update()
 {
-
+	m_coreSystem->update();
 }
 
 void SoundManager::Shutdown()
 {
-
+	for (auto& n : L_BGM) { n.second->release(); }
+	for (auto& n : L_SFX) { n.second->release(); }
+	for (auto& n : L_UI)  { n.second->release(); }
+	m_coreSystem->release();
 }
 
-void SoundManager::PlayOneShot(const std::string& eventPath)
+void SoundManager::OneShot(const std::string& eventPath)
 {
-
+	L_BGM.find(eventPath)->second->
 }
 
-void SoundManager::SetVolume(float volume)
+void SoundManager::SetVolume(float volume) // volume: 0 ~ 1
 {
-
+	if (volume < 0){ volume = 0; }
+	m_volume = volume;
 }
+
+
