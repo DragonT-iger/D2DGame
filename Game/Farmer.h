fcc840efea8195f382d7967c39d3a6414eb86990
DT <dydyqja12345@naver.com>
2025-08-03 20:45:04 +0900

    작업했는데 왜 안올라갔지?

--- Game/Farmer.h
+++ Game/Farmer.h
@@ -39,8 +39,8 @@ private:
     GameObject* patrolArea;  float patrolAreaValue = 350.0f;
     GameObject* chaseArea;  float chaseAreaValue = 500.0f;
 
-    GameObject* alertArea;   float alertArea = 300.0f;
+    GameObject* alertArea;   float alertAreaValue = 300.0f;
 
-    GameObject* attackArea;  float attackArea = 200.0f;
+    GameObject* attackArea;  float attackAreaValue = 200.0f;
 };
 
