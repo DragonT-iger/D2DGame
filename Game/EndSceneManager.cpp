49cc01bdcef6f7626f7fc7bbee796c5f77774978
DevEuni <silverlight0522@gmail.com>
2025-08-11 21:13:50 +0900

    Ending 씬 애니메이션 클립 추가

--- Game/EndSceneManager.cpp
+++ Game/EndSceneManager.cpp
@@ -6,9 +6,17 @@ void EndSceneManager::Awake()
 {
 	m_spriteRenderer = GetOwner()->AddComponent<SpriteRenderer>();
 
-	
+	m_animator = GetOwner()->AddComponent<Animator>();
 
+	//auto title = ResourceManager::Instance().LoadAnimationClip("illust_animation_final_2.json", "title");
+	auto killed = ResourceManager::Instance().LoadAnimationClip("illust_animation_final_2.json", "killed");
+	auto starve = ResourceManager::Instance().LoadAnimationClip("illust_animation_final_2.json", "starve");
+	auto happy = ResourceManager::Instance().LoadAnimationClip("illust_animation_final_2.json", "happy");
 
+	//m_animator->AddClip("title", title, true);
+	m_animator->AddClip("killed", killed, true);
+	m_animator->AddClip("starve", starve, true);
+	m_animator->AddClip("happy", happy, true);
 }
 
 void EndSceneManager::Start()
@@ -21,17 +29,18 @@ void EndSceneManager::Start()
 		break;
 
 	case GameManager::EndReason::BabyStarved:
-		m_spriteRenderer->SetBitmap(ResourceManager::Instance().LoadTexture("BadEnding2.png"));
-
+		//m_spriteRenderer->SetBitmap(ResourceManager::Instance().LoadTexture("BadEnding2.png"));
+		m_animator->ChangeState("starve");
 		break;
 
 	case GameManager::EndReason::PlayerDead:
-		m_spriteRenderer->SetBitmap(ResourceManager::Instance().LoadTexture("BadEnding1.png"));
-
+		//m_spriteRenderer->SetBitmap(ResourceManager::Instance().LoadTexture("BadEnding1.png"));
+		m_animator->ChangeState("killed");
 		break;
 
 	case GameManager::EndReason::Happy:
-		m_spriteRenderer->SetBitmap(ResourceManager::Instance().LoadTexture("HappyEnd.png"));
+		//m_spriteRenderer->SetBitmap(ResourceManager::Instance().LoadTexture("HappyEnd.png"));
+		m_animator->ChangeState("happy");
 		break;
 
 	default:
