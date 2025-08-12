8ea4dab46ce766ae4f9654e32bae74f75fad2dc1
K-2301160066\User <dydyqja12345@naver.com>
2025-08-12 15:33:34 +0900

    나무 프리팹 및 배치

--- Game/Tree.cpp
+++ Game/Tree.cpp
@@ -1,2 +1,47 @@
 #include "pch.h"
 #include "Tree.h"
+#include "YSort.h"
+
+void Tree::Awake()
+{
+	Transform* tr = GetOwner()->GetComponent<Transform>();
+	tr->SetPosition({ m_x, m_y });
+
+	tr->SetScale({ 0.5f, 0.5f });
+	m_spriteRenderer = GetOwner()->AddComponent<SpriteRenderer>();
+
+	YSort* ysort = GetOwner()->AddComponent<YSort>();
+	ysort->SetOffset(50);
+	m_boxCol = GetOwner()->AddComponent<BoxCollider>();
+
+
+
+	GetOwner()->SetTag("OutRange");
+
+	ComPtr<ID2D1Bitmap1> tree_sprite;
+
+	if (m_tree == 0) {
+
+		tree_sprite = ResourceManager::Instance().LoadTexture("tree_1.png");
+	}
+	else if (m_tree == 1) {
+
+		tree_sprite = ResourceManager::Instance().LoadTexture("tree_2.png");
+	}
+	else if (m_tree == 2) {
+
+		tree_sprite = ResourceManager::Instance().LoadTexture("tree_3.png");
+	}
+	else if (m_tree == 3) {
+
+		tree_sprite = ResourceManager::Instance().LoadTexture("tree_4.png");
+	}
+	m_spriteRenderer->SetBitmap(tree_sprite);
+
+
+	if (m_boxCol)
+	{
+		m_boxCol->SetSize(Vector2{ 100, 100 });
+		m_boxCol->SetOffset({ 5.000f, -231.000f });
+	}
+}
