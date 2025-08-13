15a4ff9544b6c43f16a415788a4b2fbedadf2568
DevEuni <silverlight0522@gmail.com>
2025-08-13 15:03:01 +0900

    safe_zone_bush 추가

--- Game/SafeZone.cpp
+++ Game/SafeZone.cpp
@@ -7,15 +7,29 @@ void SafeZone::Awake()
 {
 	Transform* tr = GetOwner()->GetComponent<Transform>();
 	tr->SetPosition({ m_x, m_y });
-	tr->Translate({ 0 , 200 });
 
-	tr->SetScale({ 0.5f, 0.5f });
+	tr->SetScale({ 1.0f, 1.0f });
 	m_spriteRenderer = GetOwner()->AddComponent<SpriteRenderer>();
 
 
 	ComPtr<ID2D1Bitmap1> tree_sprite;
 
-	tree_sprite = ResourceManager::Instance().LoadTexture("safe_zone.png");
+	tree_sprite = ResourceManager::Instance().LoadTexture("safe_zone_bush.png");
 	m_spriteRenderer->SetBitmap(tree_sprite);
-
 }
+
+//void SafeZone::OnTriggerEnter(Collider* other)
+//{
+//	if (other->GetOwner()->GetName() == "Player")
+//	{
+//		m_spriteRenderer->SetOpacity(0.3f);
+//	}
+//}
+
+//void SafeZone::OnTriggerExit(Collider* other)
+//{
+//	if (other->GetOwner()->GetName() == "Player")
+//	{
+//		m_spriteRenderer->SetOpacity(1.0f);
+//	}
+//}
